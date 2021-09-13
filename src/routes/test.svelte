a11a7b7825ed549c1b5a60e863ce728b12051ad2
Jesus Bossa <x@jesusbossa.dev>
2021-09-13 08:46:04 -0500

    chore: add test page

---
+++ src/routes/test.svelte
@@ -0,0 +1,13 @@
+<script>
+</script>
+
+<svelte:head>
+  <title>Sapper test page</title>
+</svelte:head>
+
+<h1>Hello this is a test page!</h1>
+
+<p><strong>Try editing this file (src/routes/index.svelte) to test live reloading.</strong></p>
+
+<style>
+</style>
